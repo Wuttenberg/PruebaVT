222f4aedf7d04ff8bfe7b28ee321449d27f06d18
TomasOliver <tomascampanelli@hotmail.com>
2023-05-31 14:10:57 -0300

    VINCENT PUTO
    .

--- PRUEBAVT/main.c
+++ PRUEBAVT/main.c
@@ -4,5 +4,30 @@
 int main()
 {
     printf("Aguante messi\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
+}
+
+void vincentputo();
+{
+    char control='s';
+    while(control==s)
+    {
+        printf("vincent PUTASO");
+    }
+
+
 }
